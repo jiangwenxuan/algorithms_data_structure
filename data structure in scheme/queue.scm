0cb37d4e375408f5ed502d77043e6769a7f5ab32
jwx <904653684@qq.com>
2019-05-25 10:24:52 +0800

    stack and queue

--- data structure in scheme/queue.scm
+++ data structure in scheme/queue.scm
@@ -2,22 +2,56 @@
 
 (define make-queue
   (lambda ()
-    (cons '() '())))
+    (list 'queue (cons '() '()))))
 
-(define enqueue!
-  (lambda (queue obj)
+(define queue
+  (lambda (q)
+    (if (queue? q)
+        (list-ref q 1)
+        (error "q is not queue."))))
+
+(define queue?
+  (lambda (q)
+    (cond
+      ((null? q) #f)
+      ((not (pair? q)) #f)
+      (else (eq? 'queue (list-ref q 0))))))
+
+(define queue-head
+  (lambda (q)
+    (cond
+      ((not (queue? q)) (error "q is not queue."))
+      ((queue-empty? q) (error "q is empty queue."))
+      (else (car (queue q))))))
+
+(define queue-tail
+  (lambda (q)
+    (cond
+      ((not (queue? q)) (error "q is not queue."))
+      ((queue-empty? q) (error "q is empty queue."))
+      (else (cdr (queue q))))))
+
+(define queue-empty?
+  (lambda (q)
+    (null? (car (queue q)))))
+
+(define queue-enqueue!
+  (lambda (q obj)
     (let ((lobj (cons obj '())))
-      (if (null? (car queue))
+      (if (queue-empty? q)
           (begin
-            (set-car! queue lobj)
-            (set-cdr! queue lobj))
+            (set-car! (queue q) lobj)
+            (set-cdr! (queue q) lobj))
           (begin
-            (set-cdr! (cdr queue) lobj)
-            (set-cdr! queue lobj)))
-      (car queue))))
+            (set-cdr! (queue-tail q) lobj)
+            (set-cdr! (queue q) lobj)))
+      q)))
 
-(define dequeue!
-  (lambda (queue)
-    (let ((obj (car (car queue))))
-      (set-car queue (cdr (car queue)))
-      obj)))
+(define queue-dequeue!
+  (lambda (q)
+    (let ((obj (queue-head q)))
+      (set-car! (queue q) (cdr obj))
+      (car obj))))
+
+(define (queue->list q)
+  (car (queue q)))
---
+++ data structure in scheme/stack.scm
@@ -0,0 +1,61 @@
+#lang racket
+
+(define make-stack
+  (lambda ()
+    (list 'stack)))
+
+(define stack?
+  (lambda (s)
+    (cond
+      ((null? s) #f)
+      ((not (pair? s)) #f)
+      (else (eq? 'stack (car s))))))
+
+(define stack
+  (lambda (s)
+    (if (stack? s)
+        (cdr s)
+        (error "not a stack"))))
+
+(define stack-size
+  (lambda (s)
+    (length (cdr s0))))
+
+(define stack-empty?
+  (lambda (s)
+    (null? (stack s))))
+
+(define stack-print
+  (lambda (s)
+    (display (reverse (stack s)))))
+
+(define stack-member
+  (lambda (s e)
+    (member e (stack s))))
+
+(define stack-top
+  (lambda (s)
+    (if (stack-empty? s)
+        (error "stack is empty")
+        (car (stack s)))))
+
+(define stack-pop!
+  (lambda (s)
+    (if (stack-empty? s)
+        (error "cannot pop element from a empty stack")
+        (let ((ret (car (stack s))))
+          (begin
+            (set-cdr! s (cdr (stack s)))
+            ret)))))
+
+(define stack-reverse!
+  (lambda (s)
+    (set-cdr! s (reverse (stack s)))))
+
+(define list->stack
+  (lambda (l)
+    (cons 'stack (reverse l))))
+
+(define stack->list
+  (lambda (s)
+    (reverse (stack s))))
